fc4af9a0eb6df52ac87e070303ac14d8284798a2
Ákos Kovács <akoskovacs0@gmail.com>
2025-11-08 20:23:20 +0100

    Rebrand to Finachy

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: email_address_with_name(ENV.fetch("EMAIL_SENDER", "sender@maybe.local"), "Maybe Finance")
+  default from: email_address_with_name(ENV.fetch("EMAIL_SENDER", "sender@finachy.local"), "Finachy")
   layout "mailer"
 end
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,4 +1,4 @@
-module Maybe
+module Finachy
   class << self
     def version
       Semver.new(semver)
